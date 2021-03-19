c0242ccb40d932c5b7e603c20da6b27bd6ab0ac9
haninp <haninurchairilp@gmail.com>
2021-03-19 09:23:52 +0700

    new

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,11 @@ sudo mkdir ./influx-data;
 sudo mkdir ./influx-backup;
 echo "Pre-config Done";
 docker-compose up -d;
+
+
+# post config
+# - create database [databasename]
+# - CREATE USER sysmonr WITH PASSWORD 'sysmon2013!'
+# - GRANT READ ON "[databasename]" to "sysmonr"
+# - CREATE USER sysmonw WITH PASSWORD 'sysmon2013!'
+# - GRANT WRITE ON "[databasename]" to "sysmonw"
